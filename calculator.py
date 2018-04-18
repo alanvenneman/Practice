ef7abdbc833799432e7ec3e59281cf5b25de641d
Alan Venneman <alawrencev@gmail.com>
2018-04-18 16:36:54 -0500

    gui homework and python challenge

---
+++ calculator.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+
+class Calculator():
+    def __init__(self):
+        window = Tk()
+        window.title("Calculator")
+        frame1 = Frame(window)
+        frame1.pack()
+
