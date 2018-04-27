3b541011e8aea7bf1d823bf83347cabed2c222e1
Alan Venneman <alawrencev@gmail.com>
2018-04-27 17:47:58 -0500

    project work WIP

--- Project/Mixin.py
+++ Project/Mixin.py
@@ -17,6 +17,17 @@ class Mixin(object):
 
         return -low - 1
 
+    def insertion_sort(self, sort):
+        for i in range(1, len(sort)):
+            current_element = sort[i]
+            k = i - 1
+            while k >= 0 and sort[k] > current_element:
+                sort[k + 1] = sort[k]
+                k -= 1
+
+            sort[k + 1] = current_element
+        return sort
+
     def listGeneration(self, listlength, upperbound):
         import random
 
@@ -26,3 +37,23 @@ class Mixin(object):
             li.append(random.randint(1, upperbound))
             p += 1
         return li
+
+    def build_where_clause(self, table, field, value):
+        import arcpy
+
+        field_delimited = arcpy.AddFieldDelimiters(table, field)
+        field_type = arcpy.ListFields(table, field)[0].type
+        if str(field_type) == 'String':
+            value = "'%s'" % value
+        where_clause = "%s = %s" % (field_delimited, value)
+        return where_clause
+
+    def point_in_polygon_spatial_join(self, utility, polygon, output):
+        import arcpy
+        arcpy.SpatialJoin_analysis(utility, polygon, output, "#", "#", "#", "HAVE_THEIR_CENTER_IN")
+        return output
+
+    def compare_fields(self, utility, field):
+        import arcpy
+        if field not in utility:
+            arcpy.AddField_management(utility, field_name="SUGGESTED_PROJECT_SERIAL_NUMBER", field_type="TEXT")
--- sorting.py
+++ sorting.py
@@ -16,18 +16,6 @@ class DoSort(Sort):
     def __init__(self):
         Sort.__init__(self)
 
-    def insertion_sort(self, sort):
-        for i in range(1, len(sort)):
-            current_element = sort[i]
-            k = i - 1
-            while k >= 0 and sort[k] > current_element:
-                sort[k + 1] = sort[k]
-                k -= 1
-
-            sort[k + 1] = current_element
-        return sort
-
-
 new_sort = DoSort()
 new_list = new_sort.get_list()
 insert = new_sort.insertion_sort(new_list)
