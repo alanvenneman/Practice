019139d9c4330293b87053aab7a29cc3cdfa3e20
Alan Venneman <alawrencev@gmail.com>
2018-05-02 18:20:10 -0500

    searching and sorting

--- searching.py
+++ searching.py
@@ -1,31 +1,17 @@
-def binarySearch(list1, key):
-    low = 0
-    high = len(list1) - 1
+from Project.Mixin import Mixin
 
-    while high >= low:
-        mid = (low + high) // 2
-        if key < list1[mid]:
-            high = mid - 1
-        elif key == list1[mid]:
-            return mid
-        else:
-            low = mid + 1
 
-    return -low - 1
+class Searching(Mixin):
+    def __init__(self):
+        Mixin.__init__(self)
 
-def listGeneration(listlength, upperbound):
-    import random
 
-    p = 0
-    li = []
-    while p <= listlength:
-        li.append(random.randint(1, upperbound))
-        p += 1
-    return li
+        upperbound = eval(input("Enter an integer that will be the upper bound for the random list of numbers\n"
+                                "The higher the number, the more difficult it will be to guess correctly: "))
+        number = eval(input("Search for a number between 1 and {}: ".format(upperbound)))
+        li = Mixin.listGeneration(self, 10, upperbound)
+        print("Before search: ", li)
+        print("After search:", Mixin.binarySearch(self, li, number))
 
-upperbound = eval(input("Enter an integer that will be the upper bound for the random list of numbers\nThe higher the"
-                        "number, the more difficult it will be to guess correctly: "))
-number = eval(input("Search for a number between 1 and {}: ".format(upperbound)))
-li = listGeneration(10, upperbound)
-print("Before search: ", li)
-print("After search:", binarySearch(li, number))
+
+Searching()
